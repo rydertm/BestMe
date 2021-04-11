ac5ec0e83af128dc8561c3a0bf4b48931da276c4
Henrique Costa <henriquecostapsilva@gmail.com>
2021-04-11 17:50:08 -0300

    Sistema de perguntas completo

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const questions = [
 ]
 
 const ask = ( index = 0 ) => {
-    process.stdout.write(questions[index] + "\n")
+    process.stdout.write("\n" + questions[index] + "\n\n> ")
 }
 
 ask()
 
+const answers = []
+
 process.stdin.on("data", data => {
-    process.stdout.write( data.toString().trim() + '\n\n')
-    process.exit()
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
+    }
 })
